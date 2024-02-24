720150caac268fd6c7596afb0de7eabf60cd189a
Maxence Joosten <maxence.joosten@student.hogent.be>
2024-02-24 20:24:47 +0100

    Veranderen van color
    White -> yellow

--- verslagen/Verslag Opdracht1/Installatie_Maxence.ps1
+++ verslagen/Verslag Opdracht1/Installatie_Maxence.ps1
@@ -21,50 +21,50 @@ Write-Host "
                                                                   
 "-ForegroundColor DarkGreen
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-Write-Host " ✅ | Choco Enabled" -ForegroundColor White
+Write-Host " ✅ | Choco Enabled" -ForegroundColor Yellow
 
 #Installatie programma's
 ## Essentials 
 Write-Host "『Essential Apps』" -ForegroundColor Green
 choco install -y opera-gx
-Write-Host " ✅ | Installed Opera GX" -ForegroundColor White
+Write-Host " ✅ | Installed Opera GX" -ForegroundColor Yellow
 choco install -y discord
-Write-Host " ✅ | Installed Discord" -ForegroundColor White
+Write-Host " ✅ | Installed Discord" -ForegroundColor Yellow
 choco install -y winrar
-Write-Host " ✅ | Installed winrar" -ForegroundColor White
+Write-Host " ✅ | Installed winrar" -ForegroundColor Yellow
 choco install -y notepadplusplus
-Write-Host " ✅ | Installed Notepad++" -ForegroundColor White
+Write-Host " ✅ | Installed Notepad++" -ForegroundColor Yellow
 choco install -y vlc
-Write-Host " ✅ | Installed VLC Media Player" -ForegroundColor White
+Write-Host " ✅ | Installed VLC Media Player" -ForegroundColor Yellow
 choco install -y spotify
-Write-Host " ✅ | Installed Spotify" -ForegroundColor White
+Write-Host " ✅ | Installed Spotify" -ForegroundColor Yellow
 choco install -y audacity
-Write-Host " ✅ | Installed Audacity" -ForegroundColor White
+Write-Host " ✅ | Installed Audacity" -ForegroundColor Yellow
 choco install -y libreoffice
-Write-Host " ✅ | Installed LibreOffice" -ForegroundColor White
+Write-Host " ✅ | Installed LibreOffice" -ForegroundColor Yellow
 
 ## IT Related
 Write-Host "『IT Apps』"-ForegroundColor Green
 choco install -y git
-Write-Host " ✅ | Installed Git" -ForegroundColor White
+Write-Host " ✅ | Installed Git" -ForegroundColor Yellow
 choco install -y vscode
-Write-Host " ✅ | Installed Visual Studio Code" -ForegroundColor White
+Write-Host " ✅ | Installed Visual Studio Code" -ForegroundColor Yellow
 choco install -y obsidian
-Write-Host " ✅ | Installed Obsidian" -ForegroundColor White
+Write-Host " ✅ | Installed Obsidian" -ForegroundColor Yellow
 choco install -y eclipse
-Write-Host " ✅ | Installed Eclipse" -ForegroundColor White
+Write-Host " ✅ | Installed Eclipse" -ForegroundColor Yellow
 choco install -y visualparadigm
-Write-Host " ✅ | Installed Visual Paradigm" -ForegroundColor White
+Write-Host " ✅ | Installed Visual Paradigm" -ForegroundColor Yellow
 choco install -y wireshark
-Write-Host " ✅ | Installed WireShark" -ForegroundColor White
+Write-Host " ✅ | Installed WireShark" -ForegroundColor Yellow
 choco install -y virtualbox
-Write-Host " ✅ | Installed VirtualBox" -ForegroundColor White
+Write-Host " ✅ | Installed VirtualBox" -ForegroundColor Yellow
 choco install -y gitkraken
-Write-Host " ✅ | Installed GitKraken" -ForegroundColor White
+Write-Host " ✅ | Installed GitKraken" -ForegroundColor Yellow
 choco install -y mysql.workbench
-Write-Host " ✅ | Installed MySQL Workbench" -ForegroundColor White
+Write-Host " ✅ | Installed MySQL Workbench" -ForegroundColor Yellow
 choco install -y filezilla
-Write-Host " ✅ | Installed: FileZilla" -ForegroundColor White
+Write-Host " ✅ | Installed: FileZilla" -ForegroundColor Yellow
 
 #Programma's Updaten
 Write-Host "『Upadates』"-ForegroundColor Green
